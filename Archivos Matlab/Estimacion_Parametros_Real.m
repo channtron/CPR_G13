64303b39ece88d7b145cdc72086f7122a212d0ff
AnaWhatever16 <anacasadofauli@gmail.com>
2019-10-28 18:57:04 +0100

    Faltan por estimar bien 3 parametros reales :) los valores se encuentran en Matriz_theta_y_errores

--- Archivos Matlab/Estimacion_Parametros_Real.m
+++ Archivos Matlab/Estimacion_Parametros_Real.m
@@ -5,7 +5,12 @@ clear all;
 syms m1 m2 m3 lc1 lc2 lc3 Ixx1 Ixx2 Ixx3 Iyy1 Iyy2 Iyy3 Izz1 Izz2 Izz3 Jm1 Jm2 Jm3 Bm1 Bm2 Bm3 real
 Tm=0.001;
 
-A11=2; A12=0.5; F11=6; F12=2; Ph11=0.5; Ph12=0;
+%A11=4; A12=3; F11=16; F12=1; Ph11=0.5; Ph12=0; %PARAMETRO 9
+%A11=4; A12=3; F11=6; F12=2; Ph11=0.5; Ph12=0; %PARAMETROS 7 & 8
+%A11=4; A12=3; F11=6; F12=1; Ph11=0.5; Ph12=0; %PARAMETRO 10
+%A11=3; A12=4; F11=6; F12=1; Ph11=0.5; Ph12=0; %PARAMETROS 1, 2, 3, 4, 5 & 11
+%A11=3; A12=5; F11=4; F12=1; Ph11=0.5; Ph12=0; %PARAMETR 6
+
 
 sim('sk_R3GDL_2017');
 % Factores de reducción
